0bec868cb893a93ffb817cf47f459f0d7deb9cc4
wodls1616 <wodls1616@gmail.com>
2023-09-11 13:30:26 +0900

    카테고리 정보를 보여주는 예시 코드 작성완료

--- src/main/java/com/cafe/megacoffee/category/controller/CategoryController.java
+++ src/main/java/com/cafe/megacoffee/category/controller/CategoryController.java
@@ -1,16 +1,36 @@
 package com.cafe.megacoffee.category.controller;
 
+import com.cafe.megacoffee.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/category")
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
     @GetMapping("/manage")
     public String categoryManageView(Model model) {
-        return "";
+        List<CategoryDTO> findCategory = categoryService.findParentCategoryAll();
+        model.addAttribute("category", findCategory);
+        return "/category/category";
     }
+
+    @PostMapping("/findCategory")
+    @ResponseBody
+    public List<CategoryDTO> findCategoryById(CategoryDTO categoryDTO) {
+        List<CategoryDTO> data = categoryService.findChildCategoryById(categoryDTO);
+        return data;
+    }
+  
 }
