c6b28532be7ccd36793fc334e3106220e5c72e32
wodls1616 <wodls1616@gmail.com>
2023-09-21 09:24:25 +0900

    파일 업로드 관련에서 getFullpath 메서드를 없애고 new File()에서 생성자 매개변수로 직접 지정해서 서버에 파일을 올림

--- src/main/java/com/cafe/megacoffee/util/file/FileUpload.java
+++ src/main/java/com/cafe/megacoffee/util/file/FileUpload.java
@@ -1,4 +1,4 @@
-package com.cafe.megacoffee.util;
+package com.cafe.megacoffee.util.file;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,10 +10,6 @@ import java.util.UUID;
 @Component
 public class FileUpload {
 
-    public String getFullPath(String filename, String fileDir) {
-        return fileDir + filename;
-    }
-
     public String serverUploadFile(MultipartFile multipartFile, String fileDir) throws IOException {
         if (multipartFile.isEmpty()) { //파일 없으면 null 반환
             return null;
@@ -21,8 +17,7 @@ public class FileUpload {
         String originalFilename = multipartFile.getOriginalFilename(); //원래 파일명
 
         String serverUploadFileName = createServerFileName(originalFilename); //uuid 생성해서 뒤에 원래파일명의 확장자명 붙이기
-        multipartFile.transferTo(new File(getFullPath(serverUploadFileName, fileDir)));//저장: (서버에 업로드되는 파일명, 업로드 되는 경로)
-
+        multipartFile.transferTo(new File(fileDir, serverUploadFileName));//저장: (서버에 업로드되는 파일명, 업로드 되는 경로)
         return serverUploadFileName;
     }
 
